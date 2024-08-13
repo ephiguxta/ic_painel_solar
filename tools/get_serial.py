53a448071ebdda87f65f6c3c4ab4d80c51d4d88c
ephiguxta <fabiogustavo@tuta.io>
2024-08-13 17:11:24 -0400

    correção no horário e saindo com crtl+c

--- tools/get_serial.py
+++ tools/get_serial.py
@@ -2,7 +2,7 @@ import serial
 from sys import platform
 from time import localtime
 
-baudrate = 115200
+baudrate = 9600
 port = ''
 
 if platform == 'linux':
@@ -18,18 +18,18 @@ else:
 
 ser = serial.Serial(port, baudrate)
 
-# TODO: ctrl+c pra sair do script de maneira decente
 while(True):
-	log = ser.readline()
-	log = log.decode("utf-8")
+	try:
+		log = ser.readline()
+		log = log.decode("utf-8")
 
-	time = localtime()
+		time = localtime()
 
-	hour = time.tm_hour
-	minutes = time.tm_min
-	sec = time.tm_sec
+		hour = time.tm_hour
+		minutes = time.tm_min
+		sec = time.tm_sec
 
-	# TODO: quando tiver apenas um dígio, por exemplo '15:26: 0',
-	#			cole o o zero no segundo dois pontos, ex: '15:26:0'
-	#
-	print(f"[{hour:2}:{minutes:2}:{sec:2}] {log}", end='')
+		print(f"[{hour:02}:{minutes:02}:{sec:02}] {log}", end='')
+	except KeyboardInterrupt:
+		print("\nUsuário apertou crtl+c, saindo...")
+		exit()
