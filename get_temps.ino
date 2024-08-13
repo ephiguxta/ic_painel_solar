ef75c77d43e241c056a7a5467d605f821a3a357a
giovanels <137007209+giovanels@users.noreply.github.com>
2024-08-13 14:54:30 -0400

    Implementação do DS18B20 com n_dispositivos

--- get_temps.ino
+++ get_temps.ino
@@ -1,21 +1,38 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+ 
+OneWire oneWire(23);
+DallasTemperature sensor(&oneWire);
+ 
+int ndispositivos = 0;
+float tempC;
+ 
+void setup()  {
+  sensor.begin(); 
+  Serial.begin(9600);
 
-OneWire pin(23);
-DallasTemperature bus(&pin);
-DeviceAddress sensor;
+  delay(3000);
 
-void setup(void) {
-	Serial.begin(115200);
-	bus.begin();
-	bus.getAddress(sensor, 0);
+  Serial.println("Localizando Dispositivos ..."); 
+  Serial.print("Encontrados ");
+  ndispositivos = sensor.getDeviceCount(); //metodo da biblioteca
+  Serial.print(ndispositivos, DEC);
+  Serial.println(" dispositivos.");
+  Serial.println("");
 }
-
-void loop() {
-	bus.requestTemperatures();
-
-	float temp = bus.getTempC(sensor);
-	Serial.println(temp);
-
-	delay(100);
+ 
+void loop() { 
+  sensor.requestTemperatures(); 
+ 
+  for (int i = 0;  i < ndispositivos;  i++) { //percorre o barramento até achar o nº de disp
+    Serial.print("Sensor ");
+    Serial.print(i+1);
+    Serial.print(": ");
+    tempC = sensor.getTempCByIndex(i); 
+    Serial.print(tempC);
+    Serial.println("ºC");
+  }
+  
+  Serial.println("");
+  delay(1000);
 }
