a1d2ec63d75e69705fa26a019bca9b4b4535a680
ephiguxta <fabiogustavo@tuta.io>
2024-08-13 17:05:27 -0400

    refatorando o código, diminuindo os prints

--- get_temps.ino
+++ get_temps.ino
@@ -1,38 +1,37 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
- 
+
 OneWire oneWire(23);
 DallasTemperature sensor(&oneWire);
- 
+
 int ndispositivos = 0;
 float tempC;
- 
+
 void setup()  {
-  sensor.begin(); 
-  Serial.begin(9600);
+	sensor.begin();
 
-  delay(3000);
+	Serial.begin(9600);
 
-  Serial.println("Localizando Dispositivos ..."); 
-  Serial.print("Encontrados ");
-  ndispositivos = sensor.getDeviceCount(); //metodo da biblioteca
-  Serial.print(ndispositivos, DEC);
-  Serial.println(" dispositivos.");
-  Serial.println("");
+	delay(3000);
+
+	Serial.println("Localizando Dispositivos ...");
+	Serial.print("Encontrados ");
+
+	ndispositivos = sensor.getDeviceCount();
+
+	Serial.printf("%d dispostivios\n", ndispositivos);
 }
- 
+
 void loop() { 
-  sensor.requestTemperatures(); 
- 
-  for (int i = 0;  i < ndispositivos;  i++) { //percorre o barramento até achar o nº de disp
-    Serial.print("Sensor ");
-    Serial.print(i+1);
-    Serial.print(": ");
-    tempC = sensor.getTempCByIndex(i); 
-    Serial.print(tempC);
-    Serial.println("ºC");
-  }
-  
-  Serial.println("");
-  delay(1000);
+	sensor.requestTemperatures();
+
+	// percore o barramento pegando a temperatura de cada sensor
+	for (int i = 0;  i < ndispositivos;  i++) {
+		// FIXME: tratar erro de quando o sensor não enviar a temperatura
+		tempC = sensor.getTempCByIndex(i);
+
+		Serial.printf("Sensor %d: %.2f ºC\n",i+1, tempC);
+	}
+
+	delay(1000);
 }
